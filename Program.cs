f828e1de51c1533b1568976ce8545f33fe3f9554
Daryl Dahoo <Daryl Dahoo>
2016-12-15 20:58:45 -0500

    define vars

--- Program.cs
+++ Program.cs
@@ -2,9 +2,17 @@ using System;
 
 class Program
 {
+    float tHeight,
+          pMass,
+          wMass,
+          armLength,
+          backLength,
+          sLength;
     
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
     }
+
 }
