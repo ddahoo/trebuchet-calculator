bf7986c0bf642a27bc3e2d85e4a413f8d2acf4ba
Daryl Dahoo <Daryl Dahoo>
2016-12-17 16:57:54 -0500

    Comments

--- Program.cs
+++ Program.cs
@@ -2,12 +2,12 @@ using System;
 
 class Program
 {
-    float tHeight,      
-          pMass,
-          wMass,
-          armLength,
-          backLength,
-          sLength;
+    float tHeight,      // Height of  
+          pMass,        // Mass of the projectile, in kilgrams.
+          wMass,        // Mass of the weight, in kilograms.
+          armLength,    // Length of the longest part of the arm from the pivot point, in metres.
+          backLength,   // Length of the shortest length of the arm from the pivot point, in metres.
+          sLength;      // Length of the sling, in metres.
 
     static void Main(string[] args)
     {
